0c2bbd3f3fc0767a9eb379b22ac2faae8469b303
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2021-04-08 14:52:31 +0200

    chore: re-arrange code, set proper position

--- index.js
+++ index.js
@@ -8,12 +8,35 @@ module.exports = function(md, options) {
 
 function checkboxReplace(md, options) {
   "use strict";
+  var position;
   var pattern = /\[(X|V|\s|\_|\-)\]\s(.*)/i;
-  var position = 0;
   var defaults = {
     prefix: 'checkbox-'
   };
   options = Object.assign(defaults, options);
+
+  return function(state) {
+    position = 0;
+    var blockTokens, i, j, l, token, tokens;
+    blockTokens = state.tokens;
+    j = 0;
+    l = blockTokens.length;
+    while (j < l) {
+      if (blockTokens[j].type !== "inline") {
+        j++;
+        continue;
+      }
+      tokens = blockTokens[j].children;
+      i = tokens.length - 1;
+      while (i >= 0) {
+        token = tokens[i];
+        blockTokens[j].children = tokens = md.utils.arrayReplaceAt(tokens, i, splitTextToken(token, state.Token));
+        i--;
+      }
+      j++;
+    }
+  };
+
 
   function createTokens(checked, label, Token) {
     var token = new Token("checkbox_open", "", 0);
@@ -43,25 +66,4 @@ function checkboxReplace(md, options) {
     }
     return createTokens(checked, label, Token);
   }
-
-  return function(state) {
-    var blockTokens, i, j, l, token, tokens;
-    blockTokens = state.tokens;
-    j = 0;
-    l = blockTokens.length;
-    while (j < l) {
-      if (blockTokens[j].type !== "inline") {
-        j++;
-        continue;
-      }
-      tokens = blockTokens[j].children;
-      i = tokens.length - 1;
-      while (i >= 0) {
-        token = tokens[i];
-        blockTokens[j].children = tokens = md.utils.arrayReplaceAt(tokens, i, splitTextToken(token, state.Token));
-        i--;
-      }
-      j++;
-    }
-  };
 }
